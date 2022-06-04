ccce7baaa1eb0580fbdaae71265363ef9aa9e8c3
PuneetSivananda <psivananda@chatsoft.com>
2022-06-04 21:27:00 +0530

    feat: updates to graphql code

--- src/server.js
+++ src/server.js
@@ -1,25 +1,30 @@
-import {serve} from "std/http/server.ts"
-import {graphql} from "./api.js"
+import { serve } from 'std/http/server.ts';
+import { graphql, org } from './api.js';
 
-const routes = {
-    "/":serveStatic("./app/public/index.html", "text/html"),
-    "/favicon.png":serveStatic("./app/public/favicon.png", "image/png"),
-    "/build/bundle.css":serveStatic("./app/public/build/bundle.css", "text/css"),
-    "/build/bundle.js":serveStatic("./app/public/build/bundle.js", "text/javascript"),
-    "/graphql": graphql
-}
+/**
+ * @param {Request} reg
+ * @returns {Response}
+ */
+serve((req) => {
+	const routes = {
+		'/': serveStatic('./app/public/index.html', 'text/html'),
+		'/favicon.png': serveStatic('./app/public/favicon.png', 'image/png'),
+		'/build/bundle.css': serveStatic('./app/public/build/bundle.css', 'text/css'),
+		'/build/bundle.js': serveStatic('./app/public/build/bundle.js', 'text/javascript'),
+		'/graphql': graphql
+	};
 
+	const { pathname } = new URL(req.url);
 
-serve(req=>{
-    const { pathname }=  new URL(req.url)
-    console.log(`${req.method} ${pathname}`)
-    return routes[pathname]?routes[pathname](req):routes["/"](req)
-})
+	console.log(`${req.method} ${pathname}`);
 
-function serveStatic(file, type){
-    return async ()=>{
-        new Response(await Deno.readTextFile(file),{
-            headers: { "content-type": type },
-        })
-    }
+	return routes[pathname] ? routes[pathname](req) : routes['/'](req);
+});
+
+function serveStatic(file, type) {
+	return async () => {
+		new Response(await Deno.readTextFile(file), {
+			headers: { 'content-type': type }
+		});
+	};
 }
