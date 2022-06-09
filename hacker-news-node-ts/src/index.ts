442c4e9f6d8bbf449ad3df9497ebff7b3419f812
PuneetSivananda <puneet.sivananda@gmail.com>
2022-06-10 00:18:32 +0530

    feat: test code

--- hacker-news-node-ts/src/index.ts
+++ hacker-news-node-ts/src/index.ts
@@ -1,1 +1,15 @@
 import "graphql-import-node"
+import {execute, parse} from "graphql"
+import {schema} from "./schema"
+
+async function main(){
+    const myQuery = parse(`query {info}`);
+    const result = await execute({
+        schema, 
+        document: myQuery,
+    })
+
+    console.log(result)
+}
+
+main()
