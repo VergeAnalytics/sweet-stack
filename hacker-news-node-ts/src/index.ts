e797b8bb89d49dfd5a76aaf5376a7b679b087c5c
PuneetSivananda <puneet.sivananda@gmail.com>
2022-06-18 11:58:22 +0530

    feat: added inbuilt graphql support

--- hacker-news-node-ts/src/index.ts
+++ hacker-news-node-ts/src/index.ts
@@ -1,14 +1,38 @@
 import "graphql-import-node"
 import fastify from "fastify"
-
+import {getGraphQLParameters, processRequest, renderGraphiQL, Request, sendResult, shouldRenderGraphiQL} from "graphql-helix"
+import {schema} from "./schema"
 
 async function main(){
     const server = fastify()
-    server.get("/", (req, res)=>{
-        res.send({test: true})
+    server.route({
+        method:["POST","GET"],
+        url: "/graphql",
+        handler: async (req, res)=>{
+            const request: Request = {
+                headers: req.headers,
+                method: req.method,
+                query: req.query,
+                body: req.body
+           }
+           if(shouldRenderGraphiQL(req)){
+            res.header("Content-Type", "text/html")
+            res.send(
+                renderGraphiQL({
+                    endpoint: "/graphql"
+                })
+            )
+            return
+           }
+           const {operationName, query, variables} = getGraphQLParameters(request)
+           const result = await processRequest({
+            request, schema, operationName, query, variables
+           })
+           sendResult(result, res.raw)
+        }
     })
-    server.listen(3000, "localhost", ()=>{
-        console.log("Server is running on http://localhost:3000")
+    server.listen({port:3000, host:"localhost"},()=>{
+        console.log("Server is running on http://localhost:3000/graphql")
     })
 }
 
